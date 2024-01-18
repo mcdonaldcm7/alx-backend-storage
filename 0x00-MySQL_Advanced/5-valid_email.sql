6e73dbb1116e1dc3ffea829ff2dee759e256d399
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-18 23:29:14 +0100

    Removed BEGIN and END

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,6 @@
 -- Creates a trigger to reset the attribute valid_email only when email has been changed
-DELIMITER $$
-CREATE TRIGGER valid AFTER UPDATE ON users
+CREATE TRIGGER valid
+AFTER UPDATE ON users
 FOR EACH ROW
-BEGIN
+	UPDATE users
 	SET valid_email = 0;
-END;
-$$
-DELIMITER ;
