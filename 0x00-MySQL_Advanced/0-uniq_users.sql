d286f543a683fe67643a2bbe79c2f514ce794acb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-18 22:32:47 +0100

    SQL script to create a simple lq-mcdonald table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create a `users` table if it doesn't already exists
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
