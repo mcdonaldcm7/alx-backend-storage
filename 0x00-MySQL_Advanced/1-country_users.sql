4fc42eb184a618807e281dd1e49cc56a2fa1cf2e
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-18 22:52:18 +0100

    A SQL script that creates a table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
--- Create a table `user` if it doesn't exists
-CREATE TABLE IF NOT EXISTS user (
+-- Create a table `users` if it doesn't exists
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255),
 	country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
