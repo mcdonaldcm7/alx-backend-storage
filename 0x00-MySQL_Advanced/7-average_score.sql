517c23746c0f89708b9fe2a104ebf5596e18ad13
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-19 05:49:42 +0100

    Added statement to update users table

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,11 +2,16 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	DECLARE average FLOAT;
+	DECLARE avg_score FLOAT;
 
-	SELECT AVG(score) INTO average
+	SELECT AVG(score) INTO avg_score
 	FROM corrections
 	WHERE user_id = user_id;
+
+	UPDATE users
+	SET average_score = avg_score
+	WHERE id = user_id;
 END;
 
 $$
+DELIMITER ;
