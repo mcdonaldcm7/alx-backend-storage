7f264fffc7efcd969c7e410dce3413c8883e9d63
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-19 07:36:34 +0100

    Creating an INDEX to optimize search

---
+++ 0x00-MySQL_Advanced/8-index_my_name.sql
@@ -0,0 +1,5 @@
+-- Creates an index `idx_name_first` on the table `names` and the first letter of `name`
+ALTER TABLE names
+MODIFY COLUMN name CHAR(1);
+
+CREATE INDEX idx_name_first ON names (name);
