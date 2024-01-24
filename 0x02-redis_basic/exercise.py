d54ae0d9a587c2374a502b3730456cf8bd9af8d3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-24 14:04:34 +0100

    Counting method calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python3
 """
-Task
-
-0. Writing string to Redis
-
-Create a 'Cache' class. In the '__init__' method, store an instance of the
-Redis client as a private variable named '_redis' (using 'redis.Redis()') and
-flush the instance using 'flushdb'.
-
-Create a 'store' method that takes a 'data' argument and returns a string. The
-method should generate a random key (e.g. using 'uuid'), store the input data
-in Redis using the random key and return the key.
-
-Type-annotate 'store' correctly. Remember that 'data' can be a 'str', 'bytes',
-'int' or 'float'.
+This module contains the class definition for Cache
 """
 import redis
 import uuid
-from typing import Union, Optional, Any, Callable
+from typing import Union, Callable, Any
 from functools import wraps
 
 
@@ -29,50 +16,90 @@ def count_calls(method: Callable[[Any, Union[str, bytes, int, float]], str]
     @wraps(method)
     def wrapper(self: "Cache", data: Union[str, bytes, int, float]) -> str:
         """
-        Increments the count for method everytime method is called
+        Keep count number of times method has been called
         """
         self._redis.incr(method.__qualname__)
         return method(self, data)
     return wrapper
 
 
-class Cache:
+def call_history(method: Callable[[Any, Union[str, bytes, int, float]], str]
+                 ) -> Callable[[Any, Union[str, bytes, int, float]], str]:
     """
-    Redis Cache class for storing and retrieving data
+    Creates and/or append the input and output of method to the list ":input"
+    and ":output" list keys respectively where the name of both list are
+    appended by the decorated function's qualified name
     """
+    @wraps(method)
+    def wrapper(self: "Cache", *args, **kwargs) -> str:
+        """
+        """
+        key_input = "{}:inputs".format(method.__qualname__)
+        key_output = "{}:outputs".format(method.__qualname__)
+        self._redis.rpush(key_input, str(args))
 
-    def __init__(self):
+        # Execute and store the result of the decorated function
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(key_output, output)
+        return output
+    return wrapper
+
+
+def replay(method):
+    """
+    """
+    @wraps(method)
+    def wrapper(self):
+        """
+        """
+        key_input = "{}:inputs".format(method.__qualname__)
+        key_output = "{}:outputs".format(method.__qualname__)
+        inputs = self.get(key_input)
+        outputs = self.get(key_output)
+        print("Inputs are {}".format(inputs))
+        print("Outputs are {}".format(outputs))
+    return wrapper
+
+
+class Cache:
+    def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Stores data with a key and returns the key used
+        Generate a random key using uuid, store the input data in Redis using
+        the random key and returns the key.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key, fn=None) -> Union[Any, None]:
+    def get(self, key: str, fn=None):
         """
-        Returns the value associated with key (if any), after converting it to
-        the appropriate type using fn (if provided)
+        Retrieves the data from the redis server associated with the key passed
+        and uses the callable function (if passed) fn to convert the data back
+        to the desired format
         """
         if self._redis.exists(key):
-            if fn is not None:
-                return fn(self._redis.get(key))
-            return self._redis.get(key)
+            if fn is None:
+                return self._redis.get(key)
+            return fn(self._redis.get(key))
         return None
 
     def get_str(self, key: str, fn=str) -> str:
         """
-        Parametrize 'Cache.get' with the correct conversion function(str)
+        Modification of Cache.get to automatically parametrize it with the
+        string conversion function str
         """
         return self.get(key, fn)
 
     def get_int(self, key: str, fn=int) -> int:
         """
-        Parametrize 'Cache.get' with the correct conversion function(int)
+        Modification of Cache.get to automatically parametrize it with the
+        integer conversion function int
         """
         return self.get(key, fn)
